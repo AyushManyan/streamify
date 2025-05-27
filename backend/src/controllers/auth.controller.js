c520af98b4c1ecd9125671ad505bd52b85c174ef
Ayush <ayush.manyan@lpu.in>
2025-05-27 09:24:29 +0530

    adding onboarding endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -120,3 +120,57 @@ export async function logout(req, res) {
     res.clearCookie('jwt');
     res.status(200).json({success:true, message: "Logged out successfully" });
 }
+
+export async function onboard(req, res) {
+    try {
+        
+        const userId = req.user._id;
+
+        const {fullName,bio, nativeLanguage, learningLanguage, location} = req.body;
+
+        if (!fullName || !bio || !nativeLanguage || !learningLanguage || !location) {
+            return res.status(400).json({
+                message: "All fields are required",
+                missingFields: [
+                    !fullName && "fullName",
+                    !bio && "bio",
+                    !nativeLanguage && "nativeLanguage",
+                    !learningLanguage && "learningLanguage",
+                    !location && "location"
+                ].filter(Boolean),
+             });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {
+            ...req.body,
+            isOnboarded: true
+        },{ new: true });
+
+        if (!updatedUser) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        // todo update stream user
+
+        try {
+            await upsertStreamUser({
+                id: updatedUser._id.toString(),
+                name: updatedUser.fullName,
+                image: updatedUser.profilePic || ""
+            });
+            console.log("Stream user updated successfully:", updatedUser.fullName);
+        } catch (error) {
+            console.error("Error updating Stream user during onboarding:", error);
+            return res.status(500).json({ message: "Failed to update user in Stream" });
+        }
+
+        res.status(200).json({
+            success: true,
+            user: updatedUser,
+        });
+    } catch (error) {
+        console.error("Error during onboarding:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+    
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './ilb/db.js';
@@ -9,6 +10,7 @@ const PORT = process.env.PORT;
 
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth",authRoutes)
 
